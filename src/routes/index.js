1ee7d82cfe66e6cbcc092ee8939bb6e4eeb0b6a2
Letícia Barbosa Silva <leticiabarbosasilva94@gmail.com>
2019-12-14 05:21:00 -0300

    Routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
-import Main from '../pages/Main';
+import SignIn from '../pages/SignIn';
+import SignUp from '../pages/SignUp';
+import Dashboard from '../pages/Dashboard';
+import Profile from '../pages/Profile';
+
 import Page404 from '../pages/Page404';
 
 export default function Routes() {
   return (
     <Switch>
-      <Route exact path="/" component={Main} />
+      <Route exact path="/" component={SignIn} />
+      <Route path="/register" component={SignUp} />
+
+      <Route path="/dashboard" component={Dashboard} />
+      <Route path="/profile" component={Profile} />
       <Route path="*" component={Page404} />
     </Switch>
   );
