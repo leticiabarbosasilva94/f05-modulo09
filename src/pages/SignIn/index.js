7c17ea8974bc90eead084579c6681ce12bb8632f
Letícia Barbosa Silva <leticiabarbosasilva94@gmail.com>
2019-12-14 13:34:13 -0300

    Styled auth

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import logo from '../../assets/images/logo.svg';
 
@@ -6,6 +7,14 @@ export default function Dashboard() {
   return (
     <>
       <img src={logo} alt="GoBarber" />
+
+      <form>
+        <input type="email" placeholder="Seu e-mail" />
+        <input type="password" placeholder="Sua senha" />
+
+        <button type="submit">acessar</button>
+        <Link to="/register">Criar conta gratuita</Link>
+      </form>
     </>
   );
 }
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,5 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-export default function Dashboard() {
-  return <div>SignUp</div>;
+import logo from '../../assets/images/logo.svg';
+
+export default function SignUp() {
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+
+      <form>
+        <input type="text" placeholder="Nome completo" />
+        <input type="email" placeholder="Seu e-mail" />
+        <input type="password" placeholder="Sua senha" />
+
+        <button type="submit">Criar conta</button>
+        <Link to="/">Já tenho login</Link>
+      </form>
+    </>
+  );
 }
