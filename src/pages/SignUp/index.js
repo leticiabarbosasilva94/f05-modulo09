a0560b98529029075ba54bdfae256e48a3edd0db
Letícia Barbosa Silva <leticiabarbosasilva94@gmail.com>
2019-12-15 08:48:18 -0300

    Styled SignUp

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -4,14 +4,40 @@ import { Link } from 'react-router-dom';
 import logo from '../../assets/images/logo.svg';
 
 export default function SignUp() {
+  const [name, setName] = React.useState('');
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
   return (
     <>
       <img src={logo} alt="GoBarber" />
 
-      <form>
-        <input type="text" placeholder="Nome completo" />
-        <input type="email" placeholder="Seu e-mail" />
-        <input type="password" placeholder="Sua senha" />
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="name"
+          value={name}
+          onChange={e => setName(e.target.value)}
+          placeholder="Nome completo"
+        />
+        <input
+          type="email"
+          name="email"
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+          placeholder="Seu e-mail"
+        />
+        <input
+          type="password"
+          name="password"
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+          placeholder="Sua senha"
+        />
 
         <button type="submit">Criar conta</button>
         <Link to="/">Já tenho login</Link>
