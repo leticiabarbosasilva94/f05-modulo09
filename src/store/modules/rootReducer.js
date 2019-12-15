35ddd950e0f2acfc295e7e47d2dd02b7ac171299
Letícia Barbosa Silva <leticiabarbosasilva94@gmail.com>
2019-12-15 11:57:15 -0300

    Added auth

--- src/store/modules/rootReducer.js
+++ src/store/modules/rootReducer.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux';
 
-import exampleReducer from './exampleModule/reducer';
+import auth from './auth/reducer';
 
 export default combineReducers({
-  example: exampleReducer
+  auth
 });
--- src/store/modules/rootSaga.js
+++ src/store/modules/rootSaga.js
@@ -1,7 +1,7 @@
 import { all } from 'redux-saga/effects';
 
-import exampleSaga from './exampleModule/sagas';
+import auth from './auth/sagas';
 
 export default function* rootSaga() {
-  return yield all([exampleSaga]);
+  return yield all([auth]);
 }
